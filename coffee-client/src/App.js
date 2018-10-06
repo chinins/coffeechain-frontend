3acc7440875db60322d874edc22bce9e43d82f76
jayaFeola <34057551+chinins@users.noreply.github.com>
2018-10-06 11:16:16 +0200

    create react app

---
+++ coffee-client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>coffee-blockchain-app</div>
+    )
+  }
+}
+
+export default App;
