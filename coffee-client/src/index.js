5468c54136ae9a4ec6d4356afae2ef14b5718103
Nathalia Rus <nathaliarus@live.fr>
2018-10-13 15:18:37 +0200

    updated dev tools

--- coffee-client/src/index.js
+++ coffee-client/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 import logger from 'redux-logger';
 
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './redux/reducers';
 import api from './redux/middlewares/api';
@@ -19,11 +19,11 @@ if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(...middlewares)
-);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(
+    applyMiddleware(...middlewares)
+  ));
 
 ReactDOM.render(
     <Provider store={store}>
