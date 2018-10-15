ecdd8d5c5a4619e808c77f69543a69fa7947e6ca
Adrià Pallejà <adria.palleja@gmail.com>
2018-10-15 16:30:17 +0200

    refactor: use localhost to test connections

--- coffee-client/src/constants/connections.js
+++ coffee-client/src/constants/connections.js
@@ -2,7 +2,7 @@ export const mapAccessToken =
   'pk.eyJ1IjoiY2hpbmlucyIsImEiOiJjam4xaHdleWI0a2U4M3FueDgwM2ptN2VtIn0.r0ypAbepxGXZvEwJBR3amg';
 
 // export const BASE_URL = 'http://private-c1deb-coffeechain.apiary-mock.com';
-export const BASE_URL = 'http://192.168.1.188:4000';
+export const BASE_URL = 'http://localhost:4000';
 
 export const producersIdArr = [
   595223592125200,
