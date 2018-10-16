88fbf4f5cfe5599257994eaa8d9684cccb2cb320
jayaFeola <34057551+chinins@users.noreply.github.com>
2018-10-16 17:29:53 +0200

    create transactions component & update routing

---
+++ coffee-client/src/containers/transactions.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styled from 'react-emotion';
+
+class Transactions extends Component {
+  render () {
+    return (
+      <div>Orders Component</div>
+    );
+  }
+}
+
+export default Transactions;
