217301c2e34bdd04c98dc0341888edf2d45f907f
Nathalia Rus <nathaliarus@live.fr>
2018-10-16 14:48:21 +0200

    styling stuff

--- coffee-client/src/components/history/history-style.js
+++ coffee-client/src/components/history/history-style.js
@@ -13,6 +13,7 @@ const Order = styled('div')`
   margin-left: 10%;
   font-weight: 400;
   font-size: 1em;
+  color: black;
 `;
 
 
@@ -21,6 +22,7 @@ const Total = styled('div')`
   margin-left: 51%;
   font-weight: 400;
   font-size: 1.5em;
+  color: grey;
 `;
 
 const Hr = styled('hr')`
@@ -32,20 +34,37 @@ const Hr = styled('hr')`
   border-top: 2px solid grey;
 `;
 
+const HrSmall = styled('hr')`
+  display: block;
+  border: 0;
+  width: 53%;
+  margin-top: 2%;
+  margin-left: 10%;
+  border-top: 1px solid grey;
+`;
+
 const CoffeeBox = styled('div')`
   display: flex;
   flex-direction: column;
   overflow-x: scroll;
   height: 17%;
   width: 25%;
-  position: absolute;
   letter-spacing: -0.5px;
-  top: 26.8%;
-  right: 40%;
+  margin-left: 10%;
+  margin-bottom: 1%;
+  margin-top:1%;
   background-color: transparent;
   color: black;
-  text-align: right;
+  text-align: left;
 `;
 
+const Div = styled('div')`
+display: flex;
+flex-direction: column;
+height: 500px;;
+overflow-y: scroll;
+`
 
-export { CoffeeBox, Hr, Title, Total, Order }
+
+
+export { HrSmall, Div, CoffeeBox, Hr, Title, Total, Order }
